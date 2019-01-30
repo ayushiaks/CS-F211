8178aad81160156909f5d6d4b90f8321dc5300df
ayushiaks <ayushiaks8@gmail.com>
2019-01-30 22:19:16 +0530

    Replace dijkstra by FloydWarshall

--- Lab2/7.c
+++ Lab2/7.c
@@ -2,57 +2,39 @@
 #include<limits.h>
 
 
-int mindistance(int v, int dist[v], int visited[v]){
-	int min = INT_MAX, index;
-	for (int i = 0; i < v; ++i)
-	{
-		if(dist[i]<=min && visited[i]==0)
-			min = dist[i], index = i;
-		/* code */
-	}
-	return index;
-}
 
-void printdist(int v, int dist[v]){
-	for (int i = 0; i < v; ++i)
-	{
-		printf("%d %d\n", i, dist[i]);
-		/* code */
-	}
-}
+int floydWarshall (int v, int g[v][v], int uni[v]) 
+{ 
+    int dist[v][v], i, j, k;
+   
+     for (i = 0; i < v; i++) 
+        for (j = 0; j < v; j++) 
+            dist[i][j] = g[i][j]; 
+  
+     for (k = 0; k < v; k++) 
+    { 
+         for (i = 0; i < v; i++) 
+        { 
+        	
+            for (j = 0; j < v; j++) 
+            { 
+                if (dist[i][k] + dist[k][j] < dist[i][j]) 
+                    {
+                    	dist[i][j] = dist[i][k] + dist[k][j]; 
+                    	
+                    
+                    }
+            } 
+        } 
+    }
 
-int dijsktra(int v, int g[v][v], int uni[v]){
-	int dist[v], visited[v];
-	for (int i = 0; i < v; ++i)
-	{
-		dist[i] = INT_MAX, visited[i] = 0;
-		/* code */
-	}
-	dist[0] = 0;
-	for (int i = 0; i < v-1; ++i)
-	{
-		int u = mindistance(v, dist, visited);
-		visited[u] = 1;
-		
-		for (int w = 0; w < v; ++w)
-		{
-		if(visited[w]==0 && g[u][w]>0 && dist[u]!=INT_MAX && dist[u]+g[u][w]<dist[w])
-			{
-				dist[w] = dist[u]+g[u][w];
-				printf("%d\n", w);
-			}
-		/* code */
-	}
-	
-	// return;
-	}
 	int min = INT_MAX, in;
-	printdist(v, dist);
+	// printdist(v, dist);
 	for (int i = 0; i < v; ++i)
 		{
-			if(dist[i]<min && uni[i] == 1)
-				min = dist[i], in =i;
-			if(dist[i] = min && i<in && uni[i] == 1)
+			if(dist[0][i]<min && uni[i] == 1)
+				min = dist[0][i], in =i;
+			if(dist[0][i] = min && i<in && uni[i] == 1)
 				in = i;
 
 			/* code */
@@ -112,11 +94,11 @@ int main(){
 	}
 	for (int i = 0; i < v; ++i)
 	{
-		printf("%d", uni[i]);
+		// printf("%d", uni[i]);
 		/* code */
 	}
 	
-	printf("%d\n", dijsktra(v, g, uni));
+	printf("%d\n", floydWarshall(v, g, uni));
 }
 
 /*
