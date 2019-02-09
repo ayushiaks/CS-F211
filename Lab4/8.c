d38eb64792a92e4ee8a0bda30604dc9aeeec5d25
ayushiaks <ayushiaks8@gmail.com>
2019-02-09 15:18:57 +0530

    Solved: Binary Search
    commits)

--- Lab4/8.c
+++ Lab4/8.c
@@ -1,44 +1,58 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<limits.h>
 
 #define p(x) printf("%d\n", x);
-int n, arr[100000],  x;
+#define p2(x, y, z) printf("%d %d %d\n", x, y, z);
+#define s(x) scanf("%d", &x);
+#define min(a, b) (a<b?a:b);
+
+int pivot, n, arr[100000], x, mini= INT_MAX;
 
 int binarysearch(int lo, int hi){
 	int mid = (lo+hi)/2;
-	// p(mid)
-	if(lo>hi)
-		{
-			printf("Sasuke, %d\n", lo);
-			exit(0);
-		}
 	
-	if(arr[mid]==x){
-		return mid;
-		
+	// p(pivot);	
+	if(hi < 0){
+		// printf("hi<0 ");
+		// p2(lo, mid, hi);
+		hi = n-1;
+		return binarysearch(pivot, hi);
+	}
+	if(lo>hi){
+		// printf("lo>hi ");
+		// p2(lo, mid, hi);
+		lo = 0;
+		return binarysearch(lo, pivot-1);
 	}
 	
+	if(arr[mid] == x){
+		return mid;
+	}
 	if(arr[mid]>x){
+		// printf("mid>x ");
+		// p2(lo, mid, hi);
 		return binarysearch(lo, mid-1);
 	}
 	if(arr[mid]<x){
+		// printf("mid<x ");
+		// p2(lo, mid, hi);
 		return binarysearch(mid+1, hi);
 	}
-	
+
 }
 
 int main(){
-	scanf("%d", &n);
+	s(n);
 	for (int i = 0; i < n; ++i)
 	{
-		scanf("%d", &arr[i]);
+		s(arr[i]);
+		mini = min(mini, arr[i]);
+		if(arr[i]==mini)
+			pivot = i;
 		/* code */
 	}
-	
-	scanf("%d", &x);
-	printf("Naruto, %d\n", binarysearch(0, n-1));
-	// p(ans)
-	
-
-	
+	s(x);
+	// pivot = binarysearch(0, n-1);
+	p(binarysearch(0, n-1)+1);
+	// p(pivot)
 }
--- Lab4/9.c
+++ Lab4/9.c
@@ -1,42 +1,30 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-int n, arr[100000], mid;
+#define p(x) printf("%d\n", x);
+int n, arr[100000],  x;
 
 int binarysearch(int lo, int hi){
-	int ans;
-	mid = (lo+hi)/2;
-	if(lo==hi)
-		return arr[lo];
-	if(lo<0)
-		return arr[hi];
-	if(hi>=n)
-		return arr[lo];
-	if(mid%2==0)
-	{
-		if(arr[mid] == arr[mid+1]){
-			return binarysearch(mid+2, hi);
+	int mid = (lo+hi)/2;
+	// p(mid)
+	if(lo>hi)
+		{
+			printf("Sasuke, %d\n", lo);
+			exit(0);
 		}
-		else
-			return binarysearch(lo, mid);
+	
+	if(arr[mid]==x){
+		return mid;
+		
 	}
-	else{
-		if(arr[mid-1] == arr[mid]){
-			return binarysearch(mid+1, hi);
-		}
-		else
-			return binarysearch(lo, mid-1);
+	
+	if(arr[mid]>x){
+		return binarysearch(lo, mid-1);
 	}
-
-}
-//Alternative
-void xor(){
-	int res = 0;
-	for (int i = 0; i < n; ++i)
-	{
-		res = res ^ arr[i];
-		/* code */
+	if(arr[mid]<x){
+		return binarysearch(mid+1, hi);
 	}
-	printf("%d\n", res);
+	
 }
 
 int main(){
@@ -44,8 +32,13 @@ int main(){
 	for (int i = 0; i < n; ++i)
 	{
 		scanf("%d", &arr[i]);
+		/* code */
 	}
-	printf("%d\n", binarysearch(0, n-1));
+	
+	scanf("%d", &x);
+	printf("Naruto, %d\n", binarysearch(0, n-1));
+	// p(ans)
+	
 
-	//xor();
+	
 }
